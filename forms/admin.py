0ddf003aaa0ba7dc5b8e8fbf6be9998440675e15
danquio194 <dfquinoneso@unal.edu.co>
2023-02-02 08:57:47 -0500

    Connected models with forms. Updated templates

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from forms.models import SurveyForm
+
+admin.site.register(SurveyForm)
--- forms/views.py
+++ forms/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views import generic
 
+from .forms import Form
 # Create your views here.
 
-def home_page(request):
+class FormPage(generic.FormView):
+    form_class = Form
     template_name = 'home.html'
-    return render(request, template_name)
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Form submited")
+        return super().form_valid(form)
+
+#def home_page():
+#    template_name = 'home.html'
+#    return render(request, template_name)
 
